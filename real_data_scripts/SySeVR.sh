5f01b56927d5c49e4878f1e9dd4db381948b5f82
Reveal Developer <reveal.developer@gmail.com>
2020-06-07 15:34:41 -0400

    Paper submission update

--- real_data_scripts/SySeVR.sh
+++ real_data_scripts/SySeVR.sh
@@ -16,7 +16,7 @@ dataset=$1;
 batch_size=$2;
 model_type=$3;
 export PYTHONPATH='../Vuld_SySe/':$PYTHONPATH
-for i in 1; do
+for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30; do
 	python ../Vuld_SySe/vul_det_main.py \
 		--train_file ../data/full_experiment_real_data_processed/${dataset}-syse.json \
 		--model_path ../models/SySeVR/${dataset}_new_intra_dataset-${i}.bin \

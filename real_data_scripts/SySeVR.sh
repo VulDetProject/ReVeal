6ed93ce371e50bf1b91b8bd6654985e283b79ac0
VulDetProject <64237173+VulDetProject@users.noreply.github.com>
2020-06-07 19:45:37 -0400

    Update SySeVR.sh

--- real_data_scripts/SySeVR.sh
+++ real_data_scripts/SySeVR.sh
@@ -1,16 +1,6 @@
-#dataset=$1;
-#batch_size=$2;
-#model_type=$3;
-#export PYTHONPATH='../Vuld_SySe/':$PYTHONPATH
-#for i in 1 2 3 4 5; do
-#	python ../Vuld_SySe/vul_det_main.py \
-#		--train_file ../data/SySeVR/API_function_call-${dataset}.json ../data/SySeVR/Arithmetic_expression-${dataset}.json ../data/SySeVR/Array_usage-${dataset}.json ../data/SySeVR/Pointer_usage-${dataset}.json \
-#		--model_path ../models/SySeVR/${dataset}_intra_dataset-${i}.bin \
-#		--word_to_vec ../data/Word2Vec/li_et_al_wv \
-#		--batch_size ${batch_size} \
-#		--model_type ${model_type} --intra_dataset >> ../outputs/${dataset}-SySeVR-${model_type}.log;
-#done
-
+## Choice of dataset: devign, chrome_debian
+## batch_size 128
+## model_type bigru
 
 dataset=$1;
 batch_size=$2;

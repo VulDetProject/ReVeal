892928d21acb5c287353862442a4e47133816fb9
Saikat Chakraborty <saikatc@cs.columbia.edu>
2021-01-13 12:01:13 -0500

    Update the links of the data and models

--- data/get_data.sh
+++ data/get_data.sh
@@ -4,7 +4,7 @@ if [[ -f "$FILE" ]]; then
     echo "$FILE exists, skipping download"
 else
     # https://drive.google.com/open?id=1_8QYv3SdYfIdYL5yWu7eBD4_9ta9LD73
-    fileid="1_8QYv3SdYfIdYL5yWu7eBD4_9ta9LD73"
+    fileid="1PXfHY9OxO3NnbHqCUq3CJoTNm42G7sCZ"
     curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}" > /dev/null
     curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=${fileid}" -o ${FILE}
     rm ./cookie
--- models/get_models.sh
+++ models/get_models.sh
@@ -4,7 +4,7 @@ if [[ -f "$FILE" ]]; then
     echo "$FILE exists, skipping download"
 else
     # https://drive.google.com/open?id=1plKRZ_tJZJQR7REr0QnTbBOw49Rh2ZNQ
-    fileid="1plKRZ_tJZJQR7REr0QnTbBOw49Rh2ZNQ"
+    fileid="1NT3xs7jBeKrHX4nMoLOATOxKy_zINp-V"
     curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}" > /dev/null
     curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=${fileid}" -o ${FILE}
     rm ./cookie
